b8d86afe64f7729591a72eceb7d0493c1fd65c61
r0mad3us <69357664+r0mad3us@users.noreply.github.com>
2020-08-28 21:41:29 +0300

    Add files via upload
    fix img

--- js/main.js
+++ js/main.js
@@ -18,6 +18,6 @@ document.querySelector(".tabs__item").click();
 $('.carousel').slick({
     autoplay: true,
     autoplayspeed: 2000,
-    prevArrow: '<div class="carousel__arrow carousel__prev"><img src="/img/arrow/prev.png" alt="arrow prev" ></div>',
-    nextArrow: '<div class="carousel__arrow carousel__next"><img src="/img/arrow/next.png" alt="arrow next" ></div>',
+    prevArrow: '<div class="carousel__arrow carousel__prev"><img src="img/arrow/prev.png" alt="arrow prev" ></div>',
+    nextArrow: '<div class="carousel__arrow carousel__next"><img src="img/arrow/next.png" alt="arrow next" ></div>',
 });
