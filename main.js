4b8395552a04ba1f878c248dfa8c5b02c38f773c
r0mad3us <69357664+r0mad3us@users.noreply.github.com>
2020-08-28 21:40:28 +0300

    Add files via upload
    fixed img

---
+++ main.js
@@ -0,0 +1,23 @@
+const tabs = document.querySelectorAll(".tabs__item");
+
+tabs.forEach(elem => {
+    elem.addEventListener("click", () => {
+        tabs.forEach(otherItem => {
+            if (otherItem.classList.contains("tabs__item_active")) {
+                otherItem.classList.remove("tabs__item_active");
+            }
+        })
+        elem.classList.add("tabs__item_active")
+    });
+});
+
+//Show first tab by default
+document.querySelector(".tabs__item").click();
+
+//Slick
+$('.carousel').slick({
+    autoplay: true,
+    autoplayspeed: 2000,
+    prevArrow: '<div class="carousel__arrow carousel__prev"><img src="img/arrow/prev.png" alt="arrow prev" ></div>',
+    nextArrow: '<div class="carousel__arrow carousel__next"><img src="img/arrow/next.png" alt="arrow next" ></div>',
+});
